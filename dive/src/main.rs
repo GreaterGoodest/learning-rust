8c14d7de3083138dea46c5dbf00082994180a92e
Ryan Good <usafaryangood@gmail.com>
2021-12-10 08:11:46 -0600

    completes dive part 2

--- dive/src/main.rs
+++ dive/src/main.rs
@@ -1,31 +1,7 @@
-use std::fs;
+//mod part1;
+mod part2;
+
 fn main() {
-    //retrieve data from file
-    let filename = "input.txt";
-
-    let contents = fs::read_to_string(filename).expect("unable to read file");
-
-    //create two values: x, y
-    let mut x = 0;
-    let mut y = 0;
-
-    //split by lines
-    for movement in contents.split("\n"){
-        //split line by space
-        let line_split = movement.split(" ").collect::<Vec<&str>>();
-        if line_split.len() < 2{ continue; }
-
-        let command = line_split[0];
-        let amount = line_split[1].parse::<i32>().unwrap();
-
-        if command == "forward"{
-            x += amount;
-        }else if command == "up"{
-            y -= amount;
-        }else if command == "down"{
-            y += amount;
-        }
-    }
-        
-    println!("result: {}", x * y);
+//    part1::run();
+    part2::run();
 }

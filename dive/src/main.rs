0fac6029aadd55575ac8674463a1b5df33edb3c2
Ryan Good <usafaryangood@gmail.com>
2021-12-09 20:36:02 -0600

    adds "dive" part 1 solution

---
+++ dive/src/main.rs
@@ -0,0 +1,31 @@
+use std::fs;
+fn main() {
+    //retrieve data from file
+    let filename = "input.txt";
+
+    let contents = fs::read_to_string(filename).expect("unable to read file");
+
+    //create two values: x, y
+    let mut x = 0;
+    let mut y = 0;
+
+    //split by lines
+    for movement in contents.split("\n"){
+        //split line by space
+        let line_split = movement.split(" ").collect::<Vec<&str>>();
+        if line_split.len() < 2{ continue; }
+
+        let command = line_split[0];
+        let amount = line_split[1].parse::<i32>().unwrap();
+
+        if command == "forward"{
+            x += amount;
+        }else if command == "up"{
+            y -= amount;
+        }else if command == "down"{
+            y += amount;
+        }
+    }
+        
+    println!("result: {}", x * y);
+}
